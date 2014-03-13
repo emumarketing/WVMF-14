ed15e4fb212fd6e0261845c999b31be5d51adfba
Kyle Burger <kyleburger8299@gmail.com>
2014-03-13 15:23:05 -0700

    Changes to artist links changing them to images, added markers on map

--- js/script.js
+++ js/script.js
@@ -19,4 +19,42 @@ function mapInit() {
                     center: new google.maps.LatLng(44.043304,-123.074577),
                     zoom: 16
                    };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);} 
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var mainStage = new google.maps.Marker({
+    position: new google.maps.LatLng(44.045097,-123.077628),
+    map: map,
+    title:"Main Stage"
+	});
+
+  var amphitheater = new google.maps.Marker({
+    position: new google.maps.LatLng(44.045331,-123.074234),
+    map: map,
+    title:"Amphitheater"
+	});
+
+  var fishBowl = new google.maps.Marker({
+    position: new google.maps.LatLng(44.04496,-123.073972),
+    map: map,
+    title:"Fish Bowl Workshops"
+	});
+
+  var buzz = new google.maps.Marker({
+    position: new google.maps.LatLng(44.044829,-123.074027),
+    map: map,
+    title:"The Buzz"
+	});
+
+  var taylorLounge = new google.maps.Marker({
+    position: new google.maps.LatLng(44.045055,-123.073827),
+    map: map,
+    title:"Taylor Lounge"
+	});
+
+  var collierLawn = new google.maps.Marker({
+    position: new google.maps.LatLng(44.045232,-123.074943),
+    map: map,
+    title:"Collier Lawn"
+	});
+} 
+
