b52f893f55b1a1fca49818c7fb5c365367ea6898
Joshua Rose <joshrose@uoregon.edu>
2014-03-13 13:07:36 -0700

    Plop google map into directions section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	console.log('ready');
+  // Bind click listeners to band list
 	$('#bandlist a').click(function() {
 		console.log('click!');
 		// hide anything else in there
@@ -7,7 +7,16 @@ $(document).ready(function() {
 
 		// show the band clicked
 		bandName = this.className;
-		console.log('Searching for .profiles div.'+bandName);
 		$('.profiles div.' + bandName).stop().animate({'opacity':'1'}); 	
-	});	
+	});
+
+  // Initialize goog map
+  mapInit();
 });
+
+function mapInit() {
+  var mapOptions = {
+                    center: new google.maps.LatLng(44.043304,-123.074577),
+                    zoom: 16
+                   };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);} 
