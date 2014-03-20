0e1abceca04aca3b7038ff8ae575cbf875e51630
Kyle Burger <kyleburger8299@gmail.com>
2014-03-20 12:46:24 -0700

    Smooth Scrolling added

--- js/script.js
+++ js/script.js
@@ -11,16 +11,21 @@ $(document).ready(function() {
 		$('.profiles div.' + bandName).stop().animate({'opacity':'1'}); 	
 	});
 
-  $('#event a').click(function() {
-    console.log('click!');
-
-    // hide anything else in there
-    $('.eventInfo div').animate({'opacity':'0'});
-
-    // show the band clicked
-    eventName = this.className;
-    $('.eventInfo div.' + eventName).stop().animate({'opacity':'1'});   
-  });
+  //Smooth Scrolling
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    //calculate destination place
+    var dest = 0;
+    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+        dest = $(document).height() - $(window).height();
+    } else {
+        dest = $(this.hash).offset().top;
+    }
+    //go to destination
+    $('html,body').animate({
+        scrollTop: dest
+    }, 500, 'swing');
+});
 
   // Initialize goog map
   mapInit();
