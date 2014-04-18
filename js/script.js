842de1eeaa1a8397042a26f68ab6a981f3119735
Kyle Burger <kyleburger8299@gmail.com>
2014-04-18 08:34:40 -0700

    image compression - atrak-cutout img

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ $(document).ready(function() {
 		$('.profiles div.' + bandName).stop().animate({'opacity':'1'}); 	
 	});
 
-  //Smooth Scrolling
-  $(".scroll").click(function (event) {
-    event.preventDefault();
-    //calculate destination place
-    var dest = 0;
-    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-        dest = $(document).height() - $(window).height();
-    } else {
-        dest = $(this.hash).offset().top;
-    }
-    //go to destination
-    $('html,body').animate({
-        scrollTop: dest
-    }, 500, 'swing');
-});
+      //Smooth Scrolling
+      $(".scroll").click(function (event) {
+        event.preventDefault();
+        //calculate destination place
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        //go to destination
+        $('html,body').animate({
+            scrollTop: dest
+        }, 500, 'swing');
+    });
 
   // Initialize goog map
   mapInit();
