156dc0a473e41a19ca583dba1d68f691aa9a8cd4
Joshua Rose <joshrose@uoregon.edu>
2014-05-07 14:17:42 -0700

    Replace google map with festival map image

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
 		// show the band clicked
 		bandName = this.className;
-		$('.profiles div.' + bandName).stop().animate({'opacity':'1'}); 	
+		$('.profiles div.' + bandName).stop().animate({'opacity':'1'});
 	});
 
       //Smooth Scrolling
@@ -26,55 +26,7 @@ $(document).ready(function() {
             scrollTop: dest
         }, 500, 'swing');
     });
-
-  // Initialize goog map
-  mapInit();
 });
-
-function mapInit() {
-  var mapOptions = {
-                    center: new google.maps.LatLng(44.043304,-123.074577),
-                    zoom: 16,
-                    scrollwheel: false
-                   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var mainStage = new google.maps.Marker({
-    position: new google.maps.LatLng(44.04572,-123.077608),
-    map: map,
-    title:"Main Stage"
-	});
-
-  var amphitheater = new google.maps.Marker({
-    position: new google.maps.LatLng(44.045331,-123.074234),
-    map: map,
-    title:"Amphitheater"
-	});
-
-  var fishBowl = new google.maps.Marker({
-    position: new google.maps.LatLng(44.04496,-123.073972),
-    map: map,
-    title:"Fish Bowl Workshops"
-	});
-
-  var buzz = new google.maps.Marker({
-    position: new google.maps.LatLng(44.044829,-123.074027),
-    map: map,
-    title:"The Buzz"
-	});
-
-  var taylorLounge = new google.maps.Marker({
-    position: new google.maps.LatLng(44.045055,-123.073827),
-    map: map,
-    title:"Taylor Lounge"
-	});
-
-  var collierLawn = new google.maps.Marker({
-    position: new google.maps.LatLng(44.045232,-123.074943),
-    map: map,
-    title:"Collier Lawn"
-	});
-} 
 
 $(function() {
     $('a.popper').click(function(e) {
@@ -83,7 +35,7 @@ $(function() {
         $('.popboxSM').hide();
         $('.popbox').hide();
 
-        
+
         $(target).show('#');
 
         e.preventDefault();
